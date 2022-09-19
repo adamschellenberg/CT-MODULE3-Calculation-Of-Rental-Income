6b800e16885fa6b9950aa06a7a8a0091cf76d3dd
Adam Schellenberg <adamschellenberg93@gmail.com>
2022-09-19 12:29:29 -0700

    completed assignment

--- ObjectOrientedProgramming-Calculation-of-Rental-Income.py
+++ ObjectOrientedProgramming-Calculation-of-Rental-Income.py
@@ -1,38 +1,62 @@
-# Here we assume that we have a client coming to us asking for an automated 
-# Rental Property Calculator. Our client's name is Brandon from a company 
-# called "Bigger Pockets".
-
-# Using Object Oriented Programming create a program that will calculate the 
-# Return on Investment(ROI) for a rental property.
-
-# To calculate the ROI on a rental property:
-#   1. INCOME:
-#       - Rental Income
-#       - Allow for any extra income
-#   2. EXPENSES:
-#       - Allow to list all expenses
-#   3. CASHFLOW:
-#       - cashflow = total_income - total_expenses
-#   4. ROI:
-#       - INITIAL COSTS: (total_investment)
-#           - Down Payment
-#           - Closing Costs
-#           - Reno Budget
-#           - Allow to list any misc initial costs
-#       - annual_cashflow = cashflow * 12
-#       - roi_percentage = (annual_cashflow / total_investment) * 100
+# CODING TEMPLE - MODULE 3 - ADVANCED PYTHON - CALCULATION OF RENTAL INCOME
 
 class ROICalculator():
-    def __init__(self, income = 0, expenses = 0, initial_investment = 0):
-        self.rental_income = income
-        self.expenses = expenses
-        self.initial_investment = initial_investment
-        self.cashflow = self.rental_income - self.expenses
+    def __init__(self):
+        self.rental_income = 0
+        self.expenses = 0
+        self.initial_investment = 0
+        self.cashflow = 0
+
+    def getIncome(self):
+        entering_income = True
+        while entering_income:
+            if self.rental_income == 0:
+                    response = input('What is the total monthly rental income of this property, before expenses? ')
+                    self.rental_income = int(response)
+                    entering_income = False
+            else:
+                response = input(f'You previously entered {self.rental_income} as the monthly rental income. Is this correct? Y/N : ')
+                if response.lower() == 'n':
+                    self.rental_income = 0
+                elif response.lower() == 'y':
+                    entering_income = False
+    
+    def getExpenses(self):
+        entering_expenses = True
+        while entering_expenses:
+            if self.expenses == 0:
+                response = input('What is the total monthly expenses of this property? ')
+                self.expenses = int(response)
+                entering_expenses = False
+            else:
+                response = input(f'You previously entered {self.expenses} as the expenses. Is this correct? Y/N : ')
+                if response.lower() == 'n':
+                    self.expenses = 0
+                elif response.lower() == 'y':
+                    entering_expenses = False
+
+    def getInvestment(self):
+        entering_investments = True
+        while entering_investments:
+            if self.initial_investment == 0:
+                response = input('What is the initial investment of the property, including any renovations, down payment, closing costs, and misc fees? ')
+                self.initial_investment = int(response)
+                entering_investments = False
+            else:
+                response = input(f'You previously entered {self.initial_investment} as the initial investment. Is this correct? Y/N : ')
+                if response.lower() == 'n':
+                    self.initial_investment = 0
+                elif response.lower() == 'y':
+                    entering_investments = False
 
     def calculateROI(self):
+        self.cashflow = self.rental_income - self.expenses
         annual_cashflow = self.cashflow * 12
         roi_percentage = (annual_cashflow / self.initial_investment) * 100
         print(f'Your return of investment is {roi_percentage}%')
+
+
+ROI = ROICalculator()
 
 def run():
     rental_income = 0
@@ -46,30 +70,13 @@ def run():
             print('Goodbye. Please come back soon.')
             break
         if response.lower() == 'income':
-            if rental_income == 0:
-                rental_income = input('What is the total rental income of this property, before expenses? ')
-            else:
-                response = input(f'You previously entered {rental_income} as the rental income. Is this correct? Y/N : ')
-                if response.lower() == 'n':
-                    rental_income = 0
-                    
+            ROI.getIncome()
         if response.lower() == 'expenses':
-            if expenses == 0:
-                expenses = input('What is the total expenses of this property? ')
-            else:
-                response = input(f'You previously entered {expenses} as the expenses. Is this correct? Y/N : ')
-                if response.lower() == 'n':
-                    expenses = 0
+            ROI.getExpenses()
         if response.lower() == 'investment':
-            if initial_investment == 0:
-                initial_investment = input('What is the initial investment of the property, including any renovations, down payment, closing costs, and misc fees? ')
-            else:
-                response = input(f'You previously entered {initial_investment} as the initial investment. Is this correct? Y/N : ')
-                if response.lower() == 'n':
-                    initial_investment = 0
-        if int(rental_income) > 0 and int(expenses) > 0 and int(initial_investment) > 0:
+            ROI.getInvestment()
+        if ROI.rental_income != 0 and ROI.expenses != 0 and ROI.initial_investment != 0:
             entering_information = False
-    ROI = ROICalculator(int(rental_income), int(expenses), int(initial_investment))
     ROI.calculateROI()
     print('Thank you for using our services!')
 
